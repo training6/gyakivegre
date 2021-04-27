f97b0d1f1fb4d98860ee4e85b7b982378b055a9a
RekaKR <RekaKR@users.noreply.github.com>
2021-04-27 20:45:25 +0200

    Create Client.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import './App.css'
+import Client from './components/Client/Client'
 
 const App = () => {
   const [search, setSearch] = useState('')
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([])
 
-  console.log(data)
-
-  useEffect(() => {
+  function click() {
     fetch(`/api/clients?search=${search}`)
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => setData("dsojdsopjds"))
-  }, [])
+  }
 
   return (
     <div className="App">
       <h1>Veterinarian admin - clients</h1>
 
       <input type="text" onChange={(e) => setSearch(e.target.value)} />
-      <button disabled={search.length < 3 ? true : false}>Search</button>
+      <button disabled={search.length < 3 ? true : false} onClick={() => click()}>Search</button>
+
+      {data.map(item => <Client key={uuidv4()} item={item} />)}
 
     </div>
   )
