1df4e598b9454da50073f6834e0b14647b141928
RekaKR <RekaKR@users.noreply.github.com>
2021-04-27 20:25:29 +0200

    Update App.js w fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
+  const [search, setSearch] = useState('')
+  const [data, setData] = useState([]);
+
+  console.log(data)
+
+  useEffect(() => {
+    fetch(`/api/clients?search=${search}`)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => setData("dsojdsopjds"))
+  }, [])
 
   return (
     <div className="App">
       <h1>Veterinarian admin - clients</h1>
+
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />
+      <button disabled={search.length < 3 ? true : false}>Search</button>
+
     </div>
   )
 }
